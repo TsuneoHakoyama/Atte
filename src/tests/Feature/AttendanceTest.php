8344534d19672f75860cfc3aee0f4ad650df1fd3
Tsuneo Hakoyama <tunehako@gmail.com>
2025-08-26 23:31:02 +0900

    feat(tests): add assertions to AttendanceTest for index page

--- src/tests/Feature/AttendanceTest.php
+++ src/tests/Feature/AttendanceTest.php
@@ -17,6 +17,8 @@ class AttendanceTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertViewIs('index')
+            ->assertSee('Index page');
     }
 }
